ffc4953d3f0a0efb48ab8bef3a2b758644043367
Long Tran <trlong357@gmail.com>
2021-05-30 21:29:26 +0700

    278. Adding layout wrapper component

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>Great Quote</div>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink to="/quotes" activeClassName={classes.active}>
+              All Quote
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/new-quote" activeClassName={classes.active}>
+              Add quote
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
