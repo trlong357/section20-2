18f9374528c6a68cfe62c2c3313b541b3a3fb8d4
Long Tran <trlong357@gmail.com>
2021-05-30 21:58:35 +0700

    280. Outputting data on deatail page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,22 @@
 import { useParams, Route } from "react-router-dom";
 import { Fragment } from "react";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUM_data = [
+  { id: "q1", author: "Long", text: "abcdef" },
+  { id: "q2", author: "LongABC", text: "abcdefgasdfsd" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+  const quote = DUM_data.find((quote) => quote.id === params.quoteId);
+  if (!quote) {
+    return <p>no quote found</p>;
+  }
   return (
     <Fragment>
-      <h1>Quote Detail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
